351f4b2a05371721d86603deba0d1f765b2cee46
Mihei03 <70269913+Mihei03@users.noreply.github.com>
2024-03-25 03:06:10 +0700

    Проблемы softvc

--- so-vits-svc-fork.py
+++ so-vits-svc-fork.py
@@ -4,12 +4,12 @@ from softvc.model import VITSNet
 from softvc.utils import Preprocessor
 
 # Загрузка предварительно обученной модели
-model_path = "&&&.pth"
+model_path = r"C:\Users\mihei\Desktop\NoizeMC_e750_s12000.pth.pth"
 vits = VITSNet.from_pretrained(model_path)
 
 # Предварительная обработка аудио
 preprocessor = Preprocessor()
-audio_path = "C:\Users\mihei\Desktop\Arlekino\vocals.wav"
+audio_path = r"C:\Users\mihei\Desktop\Arlekino\vocals.wav"
 audio, sr = torchaudio.load(audio_path)
 mel = preprocessor(audio)
 
@@ -19,5 +19,5 @@ with torch.no_grad():
 
 # Сохранение преобразованного аудио
 converted_audio = preprocessor.inv_mel2audio(converted_mel)
-output_path = "C:\Users\mihei\Desktop\Arlekino\converted_audio.wav"
+output_path = r"C:\Users\mihei\Desktop\Arlekino\converted_audio.wav"
 torchaudio.save(output_path, converted_audio.squeeze(), sample_rate=sr)
