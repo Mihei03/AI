05dbd5c993d3c0f44196e27000b28e9696ea8dbc
Mihei03 <70269913+Mihei03@users.noreply.github.com>
2024-03-26 18:46:38 +0700

    Пайтон для юпитера

--- so-vits-svc-fork.py
+++ so-vits-svc-fork.py
@@ -1,23 +1,410 @@
+#@title Setup 2 (just run this once)
+ # force working-directory to so-vits-svc - this line is just for safety and is probably not required
+import tarfile
+import os
+from zipfile import ZipFile
+os.chdir(r'C:\Users\mihei\Desktop\so-vits-svc')
+# taken from https://github.com/CookiePPP/cookietts/blob/master/CookieTTS/utils/dataset/extract_unknown.py
+def extract(path):
+    if path.endswith(".zip"):
+        with ZipFile(path, 'r') as zipObj:
+           zipObj.extractall(os.path.split(path)[0])
+    elif path.endswith(".tar.bz2"):
+        tar = tarfile.open(path, "r:bz2")
+        tar.extractall(os.path.split(path)[0])
+        tar.close()
+    elif path.endswith(".tar.gz"):
+        tar = tarfile.open(path, "r:gz")
+        tar.extractall(os.path.split(path)[0])
+        tar.close()
+    elif path.endswith(".tar"):
+        tar = tarfile.open(path, "r:")
+        tar.extractall(os.path.split(path)[0])
+        tar.close()
+    elif path.endswith(".7z"):
+        import py7zr
+        archive = py7zr.SevenZipFile(path, mode='r')
+        archive.extractall(path=os.path.split(path)[0])
+        archive.close()
+    else:
+        raise NotImplementedError(f"{path} extension not implemented.")
+
+# taken from https://github.com/CookiePPP/cookietts/tree/master/CookieTTS/_0_download/scripts
+
+# megatools download urls
+win64_url = "https://megatools.megous.com/builds/builds/megatools-1.11.1.20230212-win64.zip"
+win32_url = "https://megatools.megous.com/builds/builds/megatools-1.11.1.20230212-win32.zip"
+linux_url = "https://megatools.megous.com/builds/builds/megatools-1.11.1.20230212-linux-x86_64.tar.gz"
+# download megatools
+from sys import platform
+import os
+import urllib.request
+import subprocess
+from time import sleep
+
+if platform == "linux" or platform == "linux2":
+        dl_url = linux_url
+elif platform == "darwin":
+    raise NotImplementedError('MacOS not supported.')
+elif platform == "win32":
+        dl_url = win64_url
+else:
+    raise NotImplementedError ('Unknown Operating System.')
+
+dlname = dl_url.split("/")[-1]
+if dlname.endswith(".zip"):
+    binary_folder = dlname[:-4] # remove .zip
+elif dlname.endswith(".tar.gz"):
+    binary_folder = dlname[:-7] # remove .tar.gz
+else:
+    raise NameError('downloaded megatools has unknown archive file extension!')
+
+if not os.path.exists(binary_folder):
+    print('"megatools" not found. Downloading...')
+    if not os.path.exists(dlname):
+        urllib.request.urlretrieve(dl_url, dlname)
+    assert os.path.exists(dlname), 'failed to download.'
+    extract(dlname)
+    sleep(0.10)
+    os.unlink(dlname)
+    print("Done!")
+
+
+binary_folder = os.path.abspath(binary_folder)
+
+def megadown(download_link, filename='.', verbose=False):
+    """Use megatools binary executable to download files and folders from MEGA.nz ."""
+    filename = ' --path "'+os.path.abspath(filename)+'"' if filename else ""
+    wd_old = os.getcwd()
+    os.chdir(binary_folder)
+    try:
+        if platform == "linux" or platform == "linux2":
+            subprocess.call(f'./megatools dl{filename}{" --debug http" if verbose else ""} {download_link}', shell=True)
+        elif platform == "win32":
+            subprocess.call(f'megatools.exe dl{filename}{" --debug http" if verbose else ""} {download_link}', shell=True)
+    except:
+        os.chdir(wd_old) # don't let user stop download without going back to correct directory first
+        raise
+    os.chdir(wd_old)
+    return filename
+
+import urllib.request
+from tqdm import tqdm
+import gdown
+from os.path import exists
+
+def request_url_with_progress_bar(url, filename):
+    class DownloadProgressBar(tqdm):
+        def update_to(self, b=1, bsize=1, tsize=None):
+            if tsize is not None:
+                self.total = tsize
+            self.update(b * bsize - self.n)
+    
+    def download_url(url, filename):
+        with DownloadProgressBar(unit='B', unit_scale=True,
+                                 miniters=1, desc=url.split('/')[-1]) as t:
+            filename, headers = urllib.request.urlretrieve(url, filename=filename, reporthook=t.update_to)
+            print("Downloaded to "+filename)
+    download_url(url, filename)
+
+
+def download(urls, dataset='', filenames=None, force_dl=False, username='', password='', auth_needed=False):
+    assert filenames is None or len(urls) == len(filenames), f"number of urls does not match filenames. Expected {len(filenames)} urls, containing the files listed below.\n{filenames}"
+    assert not auth_needed or (len(username) and len(password)), f"username and password needed for {dataset} Dataset"
+    if filenames is None:
+        filenames = [None,]*len(urls)
+    for i, (url, filename) in enumerate(zip(urls, filenames)):
+        print(f"Downloading File from {url}")
+        #if filename is None:
+        #    filename = url.split("/")[-1]
+        if filename and (not force_dl) and exists(filename):
+            print(f"{filename} Already Exists, Skipping.")
+            continue
+        if 'drive.google.com' in url:
+            assert 'https://drive.google.com/uc?id=' in url, 'Google Drive links should follow the format "https://drive.google.com/uc?id=1eQAnaoDBGQZldPVk-nzgYzRbcPSmnpv6".\nWhere id=XXXXXXXXXXXXXXXXX is the Google Drive Share ID.'
+            gdown.download(url, filename, quiet=False)
+        elif 'mega.nz' in url:
+            megadown(url, filename)
+        else:
+            #urllib.request.urlretrieve(url, filename=filename) # no progress bar
+            request_url_with_progress_bar(url, filename) # with progress bar
+
+import huggingface_hub
+import os
+import shutil
+
+class HFModels:
+    def __init__(self, repo = "therealvul/so-vits-svc-4.0", 
+            model_dir = "hf_vul_models"):
+        self.model_repo = huggingface_hub.Repository(local_dir=model_dir,
+            clone_from=repo, skip_lfs_files=True)
+        self.repo = repo
+        self.model_dir = model_dir
+
+        self.model_folders = os.listdir(model_dir)
+        self.model_folders.remove('.git')
+        self.model_folders.remove('.gitattributes')
+
+    def list_models(self):
+        return self.model_folders
+
+    # Downloads model;
+    # copies config to target_dir and moves model to target_dir
+    def download_model(self, model_name, target_dir):
+        if not model_name in self.model_folders:
+            raise Exception(model_name + " not found")
+        model_dir = self.model_dir
+        charpath = os.path.join(model_dir,model_name)
+
+        gen_pt = next(x for x in os.listdir(charpath) if x.startswith("G_"))
+        cfg = next(x for x in os.listdir(charpath) if x.endswith("json"))
+        try:
+          clust = next(x for x in os.listdir(charpath) if x.endswith("pt"))
+        except StopIteration as e:
+          print("Note - no cluster model for "+model_name)
+          clust = None
+
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir, exist_ok=True)
+
+        gen_dir = huggingface_hub.hf_hub_download(repo_id = self.repo,
+            filename = model_name + "/" + gen_pt) # this is a symlink
+        
+        if clust is not None:
+          clust_dir = huggingface_hub.hf_hub_download(repo_id = self.repo,
+              filename = model_name + "/" + clust) # this is a symlink
+          shutil.move(os.path.realpath(clust_dir), os.path.join(target_dir, clust))
+          clust_out = os.path.join(target_dir, clust)
+        else:
+          clust_out = None
+
+        shutil.copy(os.path.join(charpath,cfg),os.path.join(target_dir, cfg))
+        shutil.move(os.path.realpath(gen_dir), os.path.join(target_dir, gen_pt))
+
+        return {"config_path": os.path.join(target_dir,cfg),
+            "generator_path": os.path.join(target_dir,gen_pt),
+            "cluster_path": clust_out}
+
+# Example usage
+# vul_models = HFModels()
+# print(vul_models.list_models())
+# print("Applejack (singing)" in vul_models.list_models())
+# vul_models.download_model("Applejack (singing)","models/Applejack (singing)")
+os.chdir(r'C:\Users\mihei\Desktop\so-vits-svc') # force working-directory to so-vits-svc - this line is just for safety and is probably not required
+download(["https://huggingface.co/therealvul/so-vits-svc-4.0-init/resolve/main/checkpoint_best_legacy_500.pt"], filenames=["hubert/checkpoint_best_legacy_500.pt"])
+print("Finished!")
+
+
+import re
+model_url = "https://mega.nz/file/P7hWwCoQ#s0OICnRbTpcUjUIS7iQPIlYwBVelZXzm_-1LLPSUd2Y" #@param {"type": "string"}
+if "huggingface.co" in model_url.lower():
+  download([re.sub(r"/blob/","/resolve/",model_url)], 
+           filenames=[os.path.join(os.getcwd(),model_url.split("/")[-1])])
+else:
+  download([model_url])
+
+import glob, os, shutil
+from pathlib import Path
+os.makedirs('models', exist_ok=True)
+model_zip_paths = glob.glob('/content/**/*.zip', recursive=True)
+
+for model_zip_path in model_zip_paths:
+    print("extracting zip",model_zip_path)
+    output_dir = os.path.join(r'C:\Users\mihei\Desktop\so-vits-svc\models',os.path.basename(os.path.splitext(model_zip_path)[0]).replace(" ","_"))
+    
+    # clean and create output dir
+    if os.path.exists(output_dir):
+      shutil.rmtree(output_dir)
+    os.mkdir(output_dir)
+    input_base = os.path.dirname(model_zip_path)
+
+    # clean input dir (if user stopped an earlier extract and we have dirty files)
+    ckpts_pre = glob.glob(os.path.join(input_base,'**/*.pth'),recursive=True)
+    jsons_pre = glob.glob(os.path.join(input_base,'**/config.json'),recursive=True)
+    for cpkt in ckpts_pre:
+      os.remove(cpkt)
+    for json in jsons_pre:
+      os.remove(json)
+
+    # do the extract
+    extract(model_zip_path)
+    ckpts = glob.glob(os.path.join(input_base,'**/*.pth'),recursive=True)
+    jsons = glob.glob(os.path.join(input_base,'**/config.json'),recursive=True)
+    for ckpt in ckpts:
+      shutil.move(ckpt,os.path.join(output_dir,os.path.basename(ckpt)))
+    for json in jsons:
+      shutil.move(json,os.path.join(output_dir,os.path.basename(json)))
+
+
+import os
+import glob
+import json
+import copy
+import logging
+import io
+from ipywidgets import widgets
+from pathlib import Path
+from IPython.display import Audio, display
+
+os.chdir(r'C:\Users\mihei\Desktop\so-vits-svc')
+
 import torch
-import torchaudio
-from softvc.model import VITSNet
-from softvc.utils import Preprocessor
+from inference import infer_tool
+from inference import slicer
+from inference.infer_tool import Svc
+import soundfile
+import numpy as np
 
-# Загрузка предварительно обученной модели
-model_path = r"C:\Users\mihei\Desktop\NoizeMC_e750_s12000.pth.pth"
-vits = VITSNet.from_pretrained(model_path)
+MODELS_DIR = "models"
 
-# Предварительная обработка аудио
-preprocessor = Preprocessor()
-audio_path = r"C:\Users\mihei\Desktop\Arlekino\vocals.wav"
-audio, sr = torchaudio.load(audio_path)
-mel = preprocessor(audio)
+def get_speakers():
+  speakers = []
+  for _,dirs,_ in os.walk(MODELS_DIR):
+    for folder in dirs:
+      cur_speaker = {}
+      # Look for G_****.pth
+      g = glob.glob(os.path.join(MODELS_DIR,folder,'G_*.pth'))
+      if not len(g):
+        print("Skipping "+folder+", no G_*.pth")
+        continue
+      cur_speaker["model_path"] = g[0]
+      cur_speaker["model_folder"] = folder
 
-# Преобразование голоса
-with torch.no_grad():
-    converted_mel = vits.convert(mel)
+      # Look for *.pt (clustering model)
+      clst = glob.glob(os.path.join(MODELS_DIR,folder,'*.pt'))
+      if not len(clst):
+        print("Note: No clustering model found for "+folder)
+        cur_speaker["cluster_path"] = ""
+      else:
+        cur_speaker["cluster_path"] = clst[0]
 
-# Сохранение преобразованного аудио
-converted_audio = preprocessor.inv_mel2audio(converted_mel)
-output_path = r"C:\Users\mihei\Desktop\Arlekino\converted_audio.wav"
-torchaudio.save(output_path, converted_audio.squeeze(), sample_rate=sr)
+      # Look for config.json
+      cfg = glob.glob(os.path.join(MODELS_DIR,folder,'*.json'))
+      if not len(cfg):
+        print("Skipping "+folder+", no config json")
+        continue
+      cur_speaker["cfg_path"] = cfg[0]
+      with open(cur_speaker["cfg_path"]) as f:
+        try:
+          cfg_json = json.loads(f.read())
+        except Exception as e:
+          print("Malformed config json in "+folder)
+        for name, i in cfg_json["spk"].items():
+          cur_speaker["name"] = name
+          cur_speaker["id"] = i
+          if not name.startswith('.'):
+            speakers.append(copy.copy(cur_speaker))
+
+    return sorted(speakers, key=lambda x:x["name"].lower())
+
+logging.getLogger('numba').setLevel(logging.WARNING)
+chunks_dict = infer_tool.read_temp("inference/chunks_temp.json")
+existing_files = []
+slice_db = -40
+wav_format = 'wav'
+
+class InferenceGui():
+  def __init__(self):
+    self.speakers = get_speakers()
+    self.speaker_list = [x["name"] for x in self.speakers]
+    self.speaker_box = widgets.Dropdown(
+        options = self.speaker_list
+    )
+    display(self.speaker_box)
+
+    def convert_cb(btn):
+      self.convert()
+    def clean_cb(btn):
+      self.clean()
+
+    self.convert_btn = widgets.Button(description="Convert")
+    self.convert_btn.on_click(convert_cb)
+    self.clean_btn = widgets.Button(description="Delete all audio files")
+    self.clean_btn.on_click(clean_cb)
+
+    self.trans_tx = widgets.IntText(value=0, description='Transpose')
+    self.cluster_ratio_tx = widgets.FloatText(value=0.0, 
+      description='Clustering Ratio')
+    self.noise_scale_tx = widgets.FloatText(value=0.4, 
+      description='Noise Scale')
+    self.auto_pitch_ck = widgets.Checkbox(value=False, description=
+      'Auto pitch f0 (do not use for singing)')
+
+    display(self.trans_tx)
+    display(self.cluster_ratio_tx)
+    display(self.noise_scale_tx)
+    display(self.auto_pitch_ck)
+    display(self.convert_btn)
+    display(self.clean_btn)
+
+  def convert(self):
+    trans = int(self.trans_tx.value)
+    speaker = next(x for x in self.speakers if x["name"] == 
+          self.speaker_box.value)
+    spkpth2 = os.path.join(os.getcwd(),speaker["model_path"])
+    print(spkpth2)
+    print(os.path.exists(spkpth2))
+
+    svc_model = Svc(speaker["model_path"], speaker["cfg_path"], 
+      cluster_model_path=speaker["cluster_path"])
+    
+    input_filepaths = [f for f in glob.glob('/content/**/*.*', recursive=True)
+     if f not in existing_files and 
+     any(f.endswith(ex) for ex in ['.wav','.flac','.mp3','.ogg','.opus'])]
+    for name in input_filepaths:
+      print("Converting "+os.path.split(name)[-1])
+      infer_tool.format_wav(name)
+
+      wav_path = str(Path(name).with_suffix('.wav'))
+      wav_name = Path(name).stem
+      chunks = slicer.cut(wav_path, db_thresh=slice_db)
+      audio_data, audio_sr = slicer.chunks2audio(wav_path, chunks)
+
+      audio = []
+      for (slice_tag, data) in audio_data:
+          print(f'#=====segment start, '
+              f'{round(len(data)/audio_sr, 3)}s======')
+          
+          length = int(np.ceil(len(data) / audio_sr *
+              svc_model.target_sample))
+          
+          if slice_tag:
+              print('jump empty segment')
+              _audio = np.zeros(length)
+          else:
+              # Padding "fix" for noise
+              pad_len = int(audio_sr * 0.5)
+              data = np.concatenate([np.zeros([pad_len]),
+                  data, np.zeros([pad_len])])
+              raw_path = io.BytesIO()
+              soundfile.write(raw_path, data, audio_sr, format="wav")
+              raw_path.seek(0)
+              _cluster_ratio = 0.0
+              if speaker["cluster_path"] != "":
+                _cluster_ratio = float(self.cluster_ratio_tx.value)
+              out_audio, out_sr = svc_model.infer(
+                  speaker["name"], trans, raw_path,
+                  cluster_infer_ratio = _cluster_ratio,
+                  auto_predict_f0 = bool(self.auto_pitch_ck.value),
+                  noice_scale = float(self.noise_scale_tx.value))
+              _audio = out_audio.cpu().numpy()
+              pad_len = int(svc_model.target_sample * 0.5)
+              _audio = _audio[pad_len:-pad_len]
+          audio.extend(list(infer_tool.pad_array(_audio, length)))
+          
+      res_path = os.path.join('/content/',
+          f'{wav_name}_{trans}_key_'
+          f'{speaker["name"]}.{wav_format}')
+      soundfile.write(res_path, audio, svc_model.target_sample,
+          format=wav_format)
+      display(Audio(res_path, autoplay=True)) # display audio file
+    pass
+
+  def clean(self):
+     input_filepaths = [f for f in glob.glob('/content/**/*.*', recursive=True)
+     if f not in existing_files and 
+     any(f.endswith(ex) for ex in ['.wav','.flac','.mp3','.ogg','.opus'])]
+     for f in input_filepaths:
+       os.remove(f)
+
+inference_gui = InferenceGui()
